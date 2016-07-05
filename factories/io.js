618272fabc6f0f8da29908dd83c2b52565c4bd89
Limjoe <linqiao416@gmail.com>
2016-07-05 14:50:36 +0800

    update io factory

--- factories/io.js
+++ factories/io.js
@@ -16,7 +16,7 @@ function setupSocket(socket) {
       updatedTime: {
         $gte: new Date().getTime() - 24 * 60 * 60 * 1000
       }
-    },'sensor updatedTime', function(err, logs) {
+    },'sensor updatedTime').sort('updatedTime').exec(function(err, logs) {
         io.emit('initData', logs);
     });
   });
@@ -26,6 +26,7 @@ module.exports = function(app) {
   var redis = require('socket.io-redis');
   io = require('socket.io')(app.get('server'));
   io.adapter(redis({ host: 'localhost', port: 6379 }));
+
   io.set('transports', ['polling']);
 
   io.on('connection', function (socket) {
